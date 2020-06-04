0b966c0f50ff51739ab6a7635108d998f19057dc
rpatricio-souza <50207058+rpatricio-souza@users.noreply.github.com>
2020-06-03 23:17:59 -0300

    Conclusão da Atividade 04

--- atividades/04_leitura.sql
+++ atividades/04_leitura.sql
@@ -1,9 +1,22 @@
+create table from_file(
+    name varchar2(20),
+    age number(3),
+    birth date
+);
+
 set serveroutput on;
+
 declare
   v_line           varchar2(32767);
   c_location       constant varchar2(80) := 'UTL_FILE_TEST';
   c_filename       constant varchar2(80) := '02_leitura.txt';
   v_handle         Utl_File.File_Type;
+  
+  linha varchar2(50); --Variavel que pega linha por linha do arquivo
+  --Variaveis que serao utilizadas para inserir na tabela criada
+  nome varchar2(20);
+  idade number(3);
+  nasc date;
   
 
   procedure Show_Is_Open is begin
@@ -32,9 +45,29 @@ begin
 
     Show_Is_Open;
     
-    
+    if(Utl_File.Is_Open(v_handle)) then
+        loop
+            begin
+                --pega uma linha do arquivo por passada no loop
+                utl_file.get_line(v_handle,linha);
+                
+                nome:=substr(linha,1,instr(linha,';')-1);
+                idade:=to_number(substr(linha,instr(linha,';')+1,2));
+                nasc:=to_date(substr(linha,instr(linha,';',1,2)+1,10),'dd/mm/yyyy');
+                
+                insert into from_file values(nome, idade, nasc);
+                
+                --Encerra a leitura quando nao encontra mais dados no arquivo
+                exception
+                    when no_data_found then
+                        exit;
+            end;
+        end loop;
+    end if;
     
     Utl_File.Fclose ( file => v_handle );
     Show_Is_Open;
 end;
 /
+
+select * from from_file;
